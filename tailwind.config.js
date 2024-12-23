0b9c01a0809441017004c848353378af20e02605
MUHAMMAD HAMZA HASHMI <110339983+hamzahashmi988@users.noreply.github.com>
2024-12-23 19:25:31 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// postcss.config.js
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
